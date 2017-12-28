a394fd949d66aedf55838e8471817b539fd85f44
jthanlopes <jthan.lopes@gmail.com>
2017-12-28 00:49:11 -0200

    Início das melhorias do sistema

--- app/Http/Controllers/CryptController.php
+++ app/Http/Controllers/CryptController.php
@@ -8,18 +8,26 @@ use Illuminate\Http\Request;
 class CryptController extends Controller
 {
   public function __construct()
-  {        
+  {
   }
 
-  public function criptografar(Request $request) {    
+  /**
+   * @param  texto a ser criptografado + chave para criptografar
+   * @return texto criptografado com openssl
+   */
+  public function criptografar(Request $request) {
     $text = $request->text;
-    $key = $request->key;    
+    $key = $request->key;
     $result = openssl_encrypt($text, "AES-128-ECB", $key);
 
     return view('resultCrypt', compact('result'));
   }
 
-  public function descriptografar(Request $request) {    
+  /**
+   * @param  texto a ser descriptografado + chave para descriptografar
+   * @return texto descriptografado
+   */
+  public function descriptografar(Request $request) {
     $text = $request->text_crypt;
     $key = $request->key_crypt;
     $result = $decrypted = openssl_decrypt($text, "AES-128-ECB", $key);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,77 +1,20 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Laravel</title>
+
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-  <!-- Styles -->
-  <style>
-    header {
-      height: 15vh;
-      font-family: sans-serif;
-    }
-    header h3 {
-      margin-left: 15px;
-    }
-    .container {
-      width: 70%;
-      margin-right: 15%;
-      margin-left: 15%;
-    }
-    * {
-      padding: 0;
-      margin: 0;
-    }
-    .box-01 {
-      width: 47.5%;
-      float: left;
-      margin-right: 2.5%;
-    }
-    .box-01 textarea {
-      width: 100%;
-      max-width: 100%;
-      min-width: 100%;
-    }
-    .box-02 {
-      width: 47.5%;
-      float: left;
-      margin-left: 2.5%;
-    }
-    .box-02 textarea {
-      width: 100%;
-      max-width: 100%;
-      min-width: 100%;
-    }
-    .box-01 input, textarea, .box-02 input, textarea {
-      position: relative;
-      width: 100%;
-      padding: 5px 0 5px 5px;
-      margin-bottom: 20px;
-    }
-    .box-01 button, .box-02 button {
-      padding: 5px;
-      width: 120px;
-      color: white;
-      background-color: green;
-      cursor: pointer;
-    }  
-    @media (max-width: 600px) 
-    {
-      .box-01 {
-        float: none;
-        width: 100%;    
-        margin: 0 0 30px 0;                
-      }
-      .box-02 {
-        float: none;
-        width: 100%;       
-        margin: 0;         
-      }
-    }            
-  </style>
+  {{--  --}}
+
+  {{-- Styles --}}
+  <link rel="stylesheet" href="">
+  {{--  --}}
+
 </head>
 <body>
   <div class="container">
@@ -92,7 +35,7 @@
     </form>
     <form method="POST" action="{{ route('descriptografar') }}">
       {{ csrf_field() }}
-      <div class="box-02">        
+      <div class="box-02">
         <textarea rows="10" cols="50" name="text_crypt" placeholder="Digite o texto a ser descriptografado" required></textarea>
         <input type="text" name="key_crypt" placeholder="Digite a chave para descriptografar" required>
         <button type="input">Descriptografar</button>
